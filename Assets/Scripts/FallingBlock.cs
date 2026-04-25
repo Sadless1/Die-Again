d27a3fc333c0ce6ce94add56e97598ecee18d12d
Sadless1 <demonnot5@gmail.com>
2026-04-25 16:48:02 +0700

    Commit to main 1

---
+++ Assets/Scripts/FallingBlock.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using UnityEngine;
+
+public class FallingBlock : MonoBehaviour
+{
+    public Rigidbody[] blocks; // nhiều cube
+    public float delay = 0.5f;
+
+    private bool isTriggered = false;
+
+    private void Start()
+    {
+        foreach (Rigidbody rb in blocks)
+        {
+            rb.isKinematic = true;
+            rb.useGravity = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !isTriggered)
+        {
+            isTriggered = true;
+            StartCoroutine(Fall());
+        }
+    }
+
+    IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(delay);
+
+        foreach (Rigidbody rb in blocks)
+        {
+            rb.isKinematic = false;
+            rb.useGravity = true;
+        }
+    }
+}
